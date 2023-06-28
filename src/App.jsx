b0b2ad4fb54c723ac9a9b950eeb1331392070253
Shivam Singh <sk811586@gmail.com>
2023-06-28 12:15:08 +0530

    added contract

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Hardhat and Tailwind</h4>
+      <h1 className="text-3xl">Hello Hardhat and Tailwind</h1>
     </div>
   )
 }
